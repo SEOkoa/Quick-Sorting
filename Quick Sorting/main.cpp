dc06b2e3915b33dd6d2973ae4e7ebeb043072791
SEOkoa <kaze11koa@gmail.com>
2021-09-17 15:20:56 +0900

    Initial Commit

---
+++ Quick Sorting/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Quick Sorting
+//
+//  Created by SEO.koa on 2021/09/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
