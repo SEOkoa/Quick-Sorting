7b0a59277f213a235aa5e6b99153ea50a6d07b46
SEOkoa <kaze11koa@gmail.com>
2021-09-18 17:55:15 +0900

    first version

--- Quick Sorting/main.cpp
+++ Quick Sorting/main.cpp
@@ -1,14 +1,121 @@
-//
-//  main.cpp
-//  Quick Sorting
-//
-//  Created by SEO.koa on 2021/09/17.
-//
-
 #include <iostream>
+#include <unistd.h>
+using namespace std;
+
+int input[100] = {0};
+
+int i, j = 0; // i = 숫자의 총 갯수 // j= 출력할 숫자의 위치
+int cnt = 0; // while문 안에서 해당 배열에 저장된 값 만큼 그래프 문자를 출력하고 초기화 하는걸 담당함.
+
+int start, pivot = 0;
+int k, l = 0;
+int tmp = 0;  // 정렬 과정에서 임시값 저장
+
+
+void notice() {
+    cout << "input numbers you want to sort (1 ~ 10, 10 numbers)\n" ;
+    cout << "And if you want to finish input, input 0 last. \n";
+    cout << "ex) 11 22 33 0 \n";
+    cout << "input : ";
+
+}
+
+void Clear() {
+    for (int i = 0; i < 100; i++) {
+        cout << endl;
+    }
+}
+
+void Display() {
+    for (j = 0; j < i; j++ ) {
+        // 막대 그래프로 출럭. 나중에 함수화 해주기
+        while (1) {
+            cout << "◼︎" ;
+            cnt++;
+    
+            if ( cnt == input[j] ) {
+                cout << endl;
+                cnt = 0;
+                break;
+                
+            }}}
+    usleep(500 * 500);
+}
+
+void Quicksort (int *input, int start, int end) {
+    
+    if (start >= end) {
+        return;
+    }
+    
+    pivot = start;
+    k = pivot + 1;  // 오른쪽으로 가면서 피벗보다 큰 값을 찾음
+    l = end;        // 왼쪽으로 가면서 피벗보다 작은 값을 찾음
+    
+    while(k <= l) {
+        
+        Clear();
+        Display();
+                
+        while ( k <= end && input[k] <= input[pivot] ) {
+            k++;
+        }
+        
+        while ( l > start && input[l] >= input[pivot]) {
+            l--;
+        }
+        
+        if ( k > l) {
+            tmp = input[l];
+            input[l] = input[pivot];
+            input[pivot] = tmp;
+            
+            Clear();
+            Display();
+
+        }
+        
+        else {
+            tmp = input[k];
+            input[k] = input[l];
+            input[l] = tmp;
+            
+            Clear();
+            Display();
+
+        }
+        
+                
+        Quicksort(input, start , l - 1);
+        Quicksort(input, l + 1, end);
+        
+        Clear();
+        Display();
+    }
+}
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+ 
+    notice();
+    
+    for (i = 0; i < 100; i++) {
+        scanf (" %d", &input[i]);
+        
+        if (input[i] == 0) {
+            break;
+        }
+    }
+    
+    Display();
+    
+    Clear();
+    
+    Quicksort(input, 0, i - 1);
+    
+    Clear();
+    Display();
+
     return 0;
 }
